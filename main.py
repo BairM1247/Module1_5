fc647564781cf59c16465581911e530c41aaf34d
BairM1247 <mba84@mail.ru>
2024-09-07 22:24:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+immutable_tuple_ = (1, 2, 'a', [1, 2, 3])
+print (immutable_tuple_)
+print ('Элементы кортежа нельзя изменить, в этом отличие от списка')
+mutable_list = [1, 2, 'ddd', 5]
+print (mutable_list)
+mutable_list [2] = 8
+print(mutable_list)
